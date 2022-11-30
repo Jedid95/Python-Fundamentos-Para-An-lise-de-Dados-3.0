75612f1ac7f5ad0750000f8c2c849725829b8cd8
Jedid95 <jedid.santos@gmail.com>
2022-11-30 23:09:21 +0000

    exercicio calculadora

--- Cap03_loops_condicionais_metodos_funcoes/Lab02/calculadora_v1.py
+++ Cap03_loops_condicionais_metodos_funcoes/Lab02/calculadora_v1.py
@@ -4,6 +4,39 @@
 # A solução será apresentada no próximo capítulo!
 # Assista o vídeo com a execução do programa!
 
-print("\n******************* Python Calculator *******************")
+print("\n******************* Python Calculator *******************\n\n")
 
+print("Selecione o número da operação desejada: \n\n")
+print("1 - Soma")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão\n")
 
+opcao = int(input("Digite sua opção (1/2/3/4): "))
+
+if opcao == 1: #Soma
+    primeiro_valor = float(input("Digite o primeiro número: "))
+    segundo_valor = float(input("Digite o segundo número: "))
+    resultado = primeiro_valor + segundo_valor
+    print(str(primeiro_valor) +" + "+ str(segundo_valor) + " = " + str(resultado) )
+
+elif opcao == 2: #Subtração
+    primeiro_valor = float(input("Digite o primeiro número: "))
+    segundo_valor = float(input("Digite o segundo número: "))
+    resultado = primeiro_valor - segundo_valor
+    print(str(primeiro_valor) +" - "+ str(segundo_valor) + " = " + str(resultado) )
+
+elif opcao == 3: #Multiplicação
+    primeiro_valor = float(input("Digite o primeiro número: "))
+    segundo_valor = float(input("Digite o segundo número: "))
+    resultado = primeiro_valor * segundo_valor
+    print(str(primeiro_valor) +" * "+ str(segundo_valor) + " = " + str(resultado) )
+
+elif opcao == 4: #Divisao
+    primeiro_valor = float(input("Digite o primeiro número: "))
+    segundo_valor = float(input("Digite o segundo número: "))
+    resultado = primeiro_valor / segundo_valor
+    print(str(primeiro_valor) +" / "+ str(segundo_valor) + " = " + str(resultado) )
+
+else:
+    print("Opção inválida")
