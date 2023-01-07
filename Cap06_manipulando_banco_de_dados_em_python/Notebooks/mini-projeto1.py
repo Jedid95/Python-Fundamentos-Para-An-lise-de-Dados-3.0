a0c4123ac7ce6b791ad2cf07cf034fb64a37654f
Jedid95 <jedid.santos@gmail.com>
2023-01-07 20:08:18 +0000

    documentacao

--- Cap06_manipulando_banco_de_dados_em_python/Notebooks/mini-projeto1.py
+++ Cap06_manipulando_banco_de_dados_em_python/Notebooks/mini-projeto1.py
@@ -40,3 +40,86 @@ import warnings
 warnings.filterwarnings("ignore")
 sns.set_theme(style = "whitegrid")
 
+
+# imdb-sqlite - comando para usar no terminal para baixar o arquivo com os dados 
+
+
+# Conecta no banco de dados
+conn = sqlite3.connect("imdb.db")
+
+# Extrai a lista de tabelas
+tabelas = pd.read_sql_query("SELECT NAME AS 'Table_Name' FROM sqlite_master WHERE type = 'table'", conn)
+
+# Tipo do objeto
+type(tabelas)
+
+# Visualiza o resultado
+tabelas.head()
+
+# Vamos converter o dataframe em uma lista
+tabelas = tabelas["Table_Name"].values.tolist()
+
+# Vamos percorrer a lista de tabelas no banco de dados e extrair o esquema de cada uma
+for tabela in tabelas:
+    consulta = "PRAGMA TABLE_INFO({})".format(tabela)
+    resultado = pd.read_sql_query(consulta, conn)
+    print("Esquema da tabela:", tabela)
+    display(resultado)
+    print("-"*100)
+    print("\n")
+
+''' 
+1- Quais São as Categorias de Filmes Mais Comuns no IMDB?
+
+Quais são os principais tipos (categorias) dos títulos (filmes)?
+'''
+
+# Cria a consulta SQL
+consulta1 = '''SELECT type, COUNT(*) AS COUNT FROM titles GROUP BY type''' 
+
+# Extrai o resultado
+resultado1 = pd.read_sql_query(consulta1, conn)
+# Visualiza o resultado
+display(resultado1)
+
+# Vamos calcular o percentual para cada tipo
+resultado1['percentual'] = (resultado1['COUNT'] / resultado1['COUNT'].sum()) * 100
+# Visualiza o resultado
+display(resultado1)
+
+# Vamos criar um gráfico com apenas 4 categorias:
+# As 3 categorias com mais títulos e 1 categoria com todo o restante
+
+# Cria um dicionário vazio
+others = {}
+# Filtra o percentual em 5% e soma o total
+others['COUNT'] = resultado1[resultado1['percentual'] < 5]['COUNT'].sum()
+# Grava o percentual
+others['percentual'] = resultado1[resultado1['percentual'] < 5]['percentual'].sum()
+# Ajusta o nome
+others['type'] = 'others'
+# Visualiza
+others
+# Filtra o dataframe de resultado
+resultado1 = resultado1[resultado1['percentual'] > 5]
+# Append com o dataframe de outras categorias
+resultado1 = resultado1.append(others, ignore_index = True)
+# Ordena o resultado
+resultado1 = resultado1.sort_values(by = 'COUNT', ascending = False)
+# Visualiza
+resultado1.head()
+
+# Apresentação para o grafico
+# Ajusta os labels
+labels = [str(resultado1['type'][i])+' '+'['+str(round(resultado1['percentual'][i],2)) +'%'+']' for i in resultado1.index]
+# Plot
+# Mapa de cores
+# https://matplotlib.org/stable/tutorials/colors/colormaps.html
+cs = cm.Set3(np.arange(100))
+# Cria a figura
+f = plt.figure()
+# Pie Plot
+plt.pie(resultado1['COUNT'], labeldistance = 1, radius = 3, colors = cs, wedgeprops = dict(width = 0.8))
+plt.legend(labels = labels, loc = 'center', prop = {'size':12})
+plt.title("Distribuição de Títulos", loc = 'Center', fontdict = {'fontsize':20,'fontweight':20})
+plt.show()
